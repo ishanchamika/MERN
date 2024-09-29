15cd606c5ec8b7966aaa0a09b9c28b5a91d9d484
ishanchamika <ishanchami9@gmail.com>
2024-09-29 22:26:50 +0530

    add route to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import MainPage from './pages/MainPage';
 import Dashboard from './pages/Dashboard';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, Router } from 'react-router-dom';
 import Register from './pages/Register';
 import Supervisor from './pages/Supervisor/Supervisor';
 import Adddetails from './pages/Supervisor/Adddetails';
 import Updatedetails from './pages/Supervisor/Updatedetails';
+import Picker_register from './pages/Supervisor/Picker_register';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 
@@ -26,6 +27,7 @@ function App()
         <Route path='/supervisor' element = {<Supervisor/>}></Route>
         <Route path='/Add_details' element = {<Adddetails/>}></Route>
         <Route path='/updatedetails' element = {<Updatedetails />}></Route>
+        <Route path='/pickerRegister' element = {<Picker_register />}></Route>
       </Routes>
     </BrowserRouter>
     </div>
